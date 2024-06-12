10979f8fc81f127ac8b7dd9d0d8ac2719b5349ec
Vedant <vedant@swamiinfotech.in>
2024-06-12 15:55:04 +0530

    Revert "Changes"
    This reverts commit b62c2f0910bc6592430cd38efe67abb13bbba606.

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Component/Layout';
+import Home from './Component/Home/Home';
+import Contact from './Component/Contact/Contact';
+import About from './Component/About/About';
+import Login from './Component/Login/Login';
+import React, { useState } from 'react';
+
+function App() {
+  const [counter, setCounter] = useState(15);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
+
+  const removeValue = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        {/* by default show component */}
+        <Route path='Layout' element={<Layout />}>
+          <Route
+            path='Home'
+            element={
+              <Home
+                counter={counter}
+                addValue={addValue}
+                removeValue={removeValue}
+              />
+            }
+          />
+          <Route path='Contact' element={<Contact />} />
+          <Route
+            path='About'
+            element={<About counter={counter} />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
